d30ae701906249c2339f14f84766119f9d67148e
MateoCardonaRincon <mcardonari@unal.edu.co>
2022-05-26 18:36:56 -0500

    Implementaiton of Part01Flux (all tests passed)

--- src/main/java/io/pivotal/literx/Part01Flux.java
+++ src/main/java/io/pivotal/literx/Part01Flux.java
@@ -1,6 +1,12 @@
 package io.pivotal.literx;
 
+import org.w3c.dom.ranges.Range;
 import reactor.core.publisher.Flux;
+
+import java.sql.SQLOutput;
+import java.time.Duration;
+import java.time.temporal.TemporalUnit;
+import java.util.List;
 
 /**
  * Learn how to create Flux instances.
@@ -14,35 +20,37 @@ public class Part01Flux {
 
 	// TODO Return an empty Flux
 	Flux<String> emptyFlux() {
-		return null;
+		return Flux.empty();
 	}
 
 //========================================================================================
 
 	// TODO Return a Flux that contains 2 values "foo" and "bar" without using an array or a collection
 	Flux<String> fooBarFluxFromValues() {
-		return null;
+		return Flux.just("foo").concatWith(Flux.just("bar"));
 	}
 
 //========================================================================================
 
 	// TODO Create a Flux from a List that contains 2 values "foo" and "bar"
 	Flux<String> fooBarFluxFromList() {
-		return null;
+		return Flux.fromIterable(List.of("foo", "bar"));
 	}
 
 //========================================================================================
 
 	// TODO Create a Flux that emits an IllegalStateException
 	Flux<String> errorFlux() {
-		return null;
+		return Flux.error(IllegalStateException::new);
 	}
 
 //========================================================================================
 
 		// TODO Create a Flux that emits increasing values from 0 to 9 each 100ms
 	Flux<Long> counter() {
-		return null;
+		return Flux.just(0L, 1L, 2L, 3L, 4L, 5L, 6L, 7L, 8L, 9L).
+				delayElements(Duration.ofMillis(100))
+				.doOnNext(e-> System.out.println("delayed element: " + e));
 	}
 
 }
